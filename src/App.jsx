58d79a488466a467a28371e09dc8d3fa7a785a1b
jsk900 <jsk900@yahoo.co.uk>
2022-03-01 11:54:51 +0100

    E08 React form inputs using refs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [formData, setFormData] = useState({ name: '', age: '', email: '' });
+
+  const nameRef = useRef('');
+  const ageRef = useRef('');
+  const emailRef = useRef('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormData({
+      name: nameRef.current.value,
+      age: ageRef.current.value,
+      email: emailRef.current.value,
+    });
+
+    nameRef.current.value = '';
+    ageRef.current.value = '';
+    emailRef.current.value = '';
+  };
+
+  console.log('rendered');
+
   return (
     <main>
-     <h1>Welcome to React</h1>
+      <h1>Welcome to React</h1>
+      <form>
+        <input type='text' placeholder='Name' ref={nameRef} />
+        <input type='text' placeholder='Age' ref={ageRef} />
+        <input type='text' placeholder='Email' ref={emailRef} />
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
+
+      <p>{formData.name}</p>
+      <p>{formData.age}</p>
+      <p>{formData.email}</p>
     </main>
   );
-}
+};
 
 export default App;
-
